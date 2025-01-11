0b97db62f50483f448de2e3122fcade279db90bc
Connor Littleton <littletonconnor@gmail.com>
2025-01-10 21:25:34 -0800

    Add more flags

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { styleText } from 'node:util';
+import { styleText } from "node:util"
 import { cli } from './cli';
-import { printHelpMessage, stout } from './utils';
+import { buildFetchOptions, printDebug, printHelpMessage, stout } from './utils';
 
 
 async function main() {
@@ -12,13 +12,18 @@ async function main() {
   };
 
   if (positionals.length !== 1) {
-    console.error(styleText('red', 'curly must be used with a single positional argument (e.g., curly [arguments] google.com)'))
+    console.error(styleText('red', '[curly] You must provide only one positional argument (e.g., curly [arguments] google.com)'))
     process.exit(1)
   }
 
   const url = positionals[0]
-  const response = await fetch(url)
-  const data = await response.json();
+  const response = await fetch(url, buildFetchOptions(values))
+  const data = values.headers?.includes('application/json') ? await response.json() : await response.text()
+
+  if (values.debug) {
+    console.log(response.headers)
+    printDebug(url, buildFetchOptions(values), response.status)
+  }
 
   stout(data)
 }

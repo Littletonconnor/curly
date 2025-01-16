99fed496a896c91e931eac0855502d058de09a5b
Connor Littleton <connorlittleton@wealthfront.com>
2025-01-16 06:02:56 -0800

    chores

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,15 @@ async function main() {
     logger().error(
       'You must provide only one positional argument (e.g., curly [arguments] google.com)',
     )
+  } else if (values.head && (values.data || values['data-raw'])) {
+    logger().error('(-d, --data) and HEAD (-I, --head) arguments were both passed.')
   }
 
   const url = positionals[0]
   const response = await curl(url, values)
 
   const data = await asyncCompute(async () => {
-    if (!values.include) {
-      return await resolveData(response)
-    }
+    return await resolveData(response)
   })
 
   stout(url, values, response, data)
